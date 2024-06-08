6048187afc40130943d667000cef00769e4b3229
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-06-08 12:43:28 +0530

    Update .gitignore to ignore venv folder and add app.py changes

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from langchain.prompts import PromptTemplate
 from langchain_community.llms import CTransformers
 
 ## Function To get response from LLAma 2 model
+MODEL_PATH="C:\Development\BlogBot\models\llama-2-7b.ggmlv3.q8_0.bin"
 
 def getLLamaresponse(input_text,no_words,blog_style):
 
     ### LLama2 model
-    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+    llm=CTransformers(model=MODEL_PATH,
                       model_type='llama',
                       config={'max_new_tokens':256,
                               'temperature':0.01})
@@ -23,13 +24,9 @@ def getLLamaresponse(input_text,no_words,blog_style):
                           template=template)
     
     ## Generate the ressponse from the LLama 2 model
-    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
+    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=int(no_words)))
     print(response)
     return response
-
-
-
-
 
 
 st.set_page_config(page_title="BlogBot",
@@ -55,4 +52,5 @@ submit=st.button("Generate")
 
 ## Final response
 if submit:
-    st.write(getLLamaresponse(input_text,no_words,blog_style))
+    response = getLLamaresponse(input_text, no_words, blog_style)
+    st.markdown(response, unsafe_allow_html=True)
